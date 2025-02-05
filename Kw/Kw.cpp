b3fd23e70133ccb39b1e5b836f4ace5139c64b60
Xamil <Xamil@XAMILIO>
2025-02-05 20:23:20 +0200

    Добавьте файлы проекта.

---
+++ Kw/Kw.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	//1
+	//const int R = 5;
+	//const int C = 5;
+	//int arr[R][C];
+	//int num;
+	//cout << "Enter number: ";
+	//cin >> num;
+	//for (int i = 0; i < R; i++)
+	//{
+	//	for (int j = 0; j < C; j++)
+	//	{
+	//		arr[i][j] = num;
+	//		num *= 2;
+	//		cout << arr[i][j] << " ";
+	//	}
+	//	cout << endl;
+	//}
+	const int R = 5;
+	const int C = 5;
+	int arr[R][C];
+	int num;
+	cout << "Enter number: ";
+	cin >> num;
+	for (int i = 0; i < R; i++)
+	{
+		for (int j = 0; j < C; j++)
+		{
+			arr[i][j] = num;
+			num += 1;
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
